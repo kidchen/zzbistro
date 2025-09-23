842910fff1d94710eca7d2672c3a9a80242a23fd
kidchen <kid.chen@outlook.com>
2025-09-23 11:11:50 -0700

    Add Vercel Speed Insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import "./globals.css";
 import "../styles/colors.css";
 import SessionProvider from "@/components/SessionProvider";
@@ -48,6 +49,7 @@ export default function RootLayout({
             </ConditionalLayout>
           </AuthWrapper>
         </SessionProvider>
+        <SpeedInsights />
       </body>
     </html>
   );
